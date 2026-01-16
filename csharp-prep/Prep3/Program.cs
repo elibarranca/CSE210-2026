d1581933c46d1019670fb28ed3437b3738cb70ef
elibarranca <elisabarranca.99@gmail.com>
2026-01-15 20:02:44 -0700

    notes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?"); //I need the answer to be right in the same line
+
+        string input = Console.ReadLine(); //This input is going to be stored in a variable, and the input is text
+                                           // the input might be numbers so I need to be able to hold that dat
+
     }
 }
